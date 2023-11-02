251a13f953ef0e7c4b01d13604042daafcdaf5c5
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2023-11-02 13:37:47 +0100

    Added code for morse decode

--- decode.rb
+++ decode.rb
@@ -10,5 +10,17 @@ def decode_char(morse_code)
       '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
     }
     morse_code_ref[morse_code]
-  end
+end
+
+def decode_word(morse_word)
+    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
+end
+
+def decode(morse_message)
+    morse_words = morse_message.split('   ')
+    morse_words.map { |morse_word| decode_word(morse_word) }.join(' ')
+end
+
+morse_message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+puts decode(morse_message)
  
